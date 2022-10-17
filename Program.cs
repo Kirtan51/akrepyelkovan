c37215a7c7c38359b1926fe5cfffe9470044adc3
Beyza <112386181+Kirtan51@users.noreply.github.com>
2022-10-18 00:11:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace akrepyelkovan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            {
+                Console.WriteLine("saati giriniz:");
+
+                int saat = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("dakika giriniz");
+
+                int dakika = Convert.ToInt32(Console.ReadLine());
+
+                double açı = (11 * dakika - 60 * saat) / 2;
+
+                Math.Abs(açı);
+
+                Console.WriteLine("açı:" + açı);
+
+            }
+        }
+    }
+}
